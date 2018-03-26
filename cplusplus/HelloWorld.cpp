e68260390f0804041123bf4717afcf1a7aa2d008
Bhupinder Singh <bhupinder.singh@ascenaretail.com>
2018-03-26 21:37:14 +0530

    Add hello world

---
+++ cplusplus/HelloWorld.cpp
@@ -0,0 +1,29 @@
+/*
+ * HelloWorld.cpp
+ *
+ *  Created on: Mar 26, 2018
+ *      Author: bsin28
+ *
+ */
+
+// Simple C++ program to display "Hello World"
+
+// Header file for input output functions
+#include<iostream>
+
+using namespace std;
+
+// main function -
+// where the execution of program begins
+int main()
+{
+	// prints heelo world
+	cout<<"Hello World";
+
+	return 0;
+}
+
+
+
+
+
